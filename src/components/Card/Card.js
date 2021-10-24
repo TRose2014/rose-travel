983bbcf03a3b390144a5a11c20cc1602c4169b0e
TRose2014 <shortyshort789@aol.com>
2021-10-24 02:00:38 -0700

    updated cards and added icon to .html

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,34 +1,36 @@
+import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { v4 as uuidv4 } from 'uuid';
+import './Card.css'
 
-export default function ActionAreaCard({data}) {
+export default function RecipeReviewCard({data}) {
   return (
-    <div style={{"display": "flex"}}>
+    <div className="cardContainer">
       {data.map((item) => (
         <Box sx={{ flexGrow: 2 }} key={uuidv4()}>
           <Grid container direction="row" justifyContent="space-around" alignItems="center">
             <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea> 
-                <CardMedia
-                  component="img"
-                  height="140"
-                  src={item.imageurl}
-                  alt={item.name}/>
+              <CardHeader
+                title={item.name}
+              />
+              <CardMedia
+                component="img"
+                height="194"
+                image={item.imageurl}
+                alt={item.name}
+              />
                 <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                  {item.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                  {item.description}
+                  <Typography paragraph>Description:</Typography>
+                  <Typography paragraph>
+                    {item.description}
                   </Typography>
                 </CardContent>
-              </CardActionArea>
             </Card>
           </Grid>
         </Box>

a6cb04fef7224c11f86a3f7da4ae98756a97e80e
TRose2014 <shortyshort789@aol.com>
2021-12-12 11:57:50 +0900

    working on onclick, updating data and footer

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,15 +7,23 @@ import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react'
 import './Card.css'
 
 export default function RecipeReviewCard({data}) {
+
+  const [displayInfo, setdisplayInfo] = useState(false)
+
+  const toggleClass = () => {
+    setdisplayInfo(!displayInfo);
+  };
+
   return (
     <div className="cardContainer">
       {data.map((item) => (
         <Box sx={{ flexGrow: 2 }} key={uuidv4()}>
           <Grid container direction="row" justifyContent="space-around" alignItems="center">
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 345 }} onClick={toggleClass}>
               <CardHeader
                 title={item.name}
               />
@@ -25,7 +33,7 @@ export default function RecipeReviewCard({data}) {
                 image={item.imageurl}
                 alt={item.name}
               />
-                <CardContent>
+                <CardContent className={displayInfo ? '.css-46bh2p-MuiCardContent-root:last-child': 'nodisplay'}>
                   <Typography paragraph>Description:</Typography>
                   <Typography paragraph>
                     {item.description}
